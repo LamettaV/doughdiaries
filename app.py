710c5ee37789971af14ce690d0f99d404bbd639f
Andrew Guadí <94252760+AndrewGuadi@users.noreply.github.com>
2024-05-20 17:37:30 +0000

    checking

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('contact')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
