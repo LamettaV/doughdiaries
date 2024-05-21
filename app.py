481c3210e69cddb1f7284da29d0afbc85191d4ff
LamettaV <119691705+LamettaV@users.noreply.github.com>
2024-05-21 17:01:38 +0000

    Changed site structure tempoarily

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def countdown():
+    return render_template('countdown.html') 
+
+# @app.route('/')
+# def index():
+#   return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -16,3 +20,4 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
