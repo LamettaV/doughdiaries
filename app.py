fed9a8fbc44c8f475c58f867250a203d5e2ac811
LamettaV <119691705+LamettaV@users.noreply.github.com>
2024-06-07 16:54:31 +0000

    Changed route

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/home')
+def index():
+   return render_template('index.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
